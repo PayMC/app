26fbcb0b55dd6188400c30688096eb601a1365c1
mattrick <social@mattrick.me>
2016-12-10 01:20:19 -0800

    Add eslint and travis support; little bit of code cleanup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PayMC
-PayMC is a free to use, self-hosted Minecraft store software. It is designed to be
-easy to setup and use and also to be attractive out of the box.
+[![Build Status](https://travis-ci.org/PayMC/app.svg?branch=master)](https://travis-ci.org/PayMC/app)
+
+PayMC is a free to use, self-hosted Minecraft store software. It is designed to be easy to setup and use and attractive 
+out of the box.
+
+## Contributing
+Be sure to run ```npm test``` before you submit a pull request to ensure we have a consistent code style throughout 
+this repository.
--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var nunjucks = require('nunjucks');
-var path = require("path");
-var fs = require("fs");
+var path = require('path');
 
 //Routes
-var store = require("./routes/store");
-var admin = require("./routes/admin");
+var store = require('./routes/store');
+var admin = require('./routes/admin');
 
 var themeDir = __dirname + '/themes/default/';
 
@@ -20,4 +19,4 @@ app.use('/admin', admin);
 
 app.use('/public', express.static('public'));
 
-app.listen(3000, "0.0.0.0");
+app.listen(3000, '0.0.0.0');
